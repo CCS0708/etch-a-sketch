24f9799848f2269e1250ffe6794df0c3109acbfe
CCS <chingchunsheng@gmail.com>
2026-04-11 16:17:58 +0800

    Add toggle random colour and darkening option

--- script.js
+++ script.js
@@ -18,9 +18,21 @@ btnClear.addEventListener('click', (e) => {
   renderGrid();
 })
 
+const btnToggleColor = document.querySelector('.toggle-color');
+btnToggleColor.addEventListener('click', (e) => {
+  btnToggleColor.classList.toggle('color');
+})
+
+const btnToggleDarkening = document.querySelector('.toggle-darkening');
+btnToggleDarkening.addEventListener('click', (e) => {
+  btnToggleDarkening.classList.toggle('darkening');
+})
+
 renderGrid();
 
 function renderGrid() {
+  const sizeDisplay = document.querySelector('.size');
+  sizeDisplay.textContent = `Size: ${size} x ${size}`;
   
   const container = document.querySelector('.container');
   container.innerHTML = '';
@@ -33,14 +45,39 @@ function renderGrid() {
     square.style.setProperty('--side', `${sidePercent}%`);
 
     square.addEventListener('mouseenter', (e) => {
-      square.classList.add('hover');
+      const color = btnToggleColor.classList.contains('color');
+      const darken = btnToggleDarkening.classList.contains('darkening');
+      const background = square.style.backgroundColor;
+      if (background) {
+
+      } else if (color) {
+        bgChange(square);
+      } else {
+        square.style.backgroundColor = 'black';
+      }
+      if (darken) {
+        opacityChange(square);
+      }
+
     })
 
     container.appendChild(square);
   }
 }
-
-
-
-
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+function bgChange(element) {
   
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  element.style.backgroundColor = rndCol;
+}
+function opacityChange(element) {
+  const opacity = Number(element.style.opacity);
+
+  if (element.style.opacity === '') {
+    element.style.opacity = '0.5';
+  } else {
+    element.style.opacity = `${opacity >= 1 ? 1 : (opacity + 0.1)}`;
+  }
+}
