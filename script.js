dd40b5e1d5aa0de2df0bad0cd473b29869a1f539
CCS <chingchunsheng@gmail.com>
2026-04-11 11:36:55 +0800

    Add set size and clear function
    Able to create maximum 100 x 100 squares

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+let size = 16;
+
+const btnSize = document.querySelector('.ask-size');
+btnSize.addEventListener('click', (e) => {
+  size = prompt('Enter squares per side:', 16);
+  if (size > 100) {
+    alert('Maximum 100 squares per side');
+    size = 100;
+  }
+  renderGrid();
+})
+
+const btnClear = document.querySelector('.clear');
+btnClear.addEventListener('click', (e) => {
+  renderGrid();
+})
+
 renderGrid();
 
 function renderGrid() {
+  
   const container = document.querySelector('.container');
   container.innerHTML = '';
 
-  for (let i = 0; i < (16 * 16); i++) {
+  const sidePercent = 100 / size; 
+
+  for (let i = 0; i < (size * size); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.style.setProperty('--side', `${sidePercent}%`);
 
     square.addEventListener('mouseenter', (e) => {
       square.classList.add('hover');
@@ -24,3 +45,7 @@ function addHover() {
     })
   })
 }
+
+
+
+  
